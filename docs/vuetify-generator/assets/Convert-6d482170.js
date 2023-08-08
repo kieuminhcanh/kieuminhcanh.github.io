ef4846b7db01ef16d677d713b744bb276241a31e
kieuminhcanh <minhcanhdn0704@gmail.com>
2023-08-08 16:26:48 -0700

    Rename repo

--- docs/vuetify-generator/assets/Convert-6d482170.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./Convert.vue_vue_type_script_setup_true_lang-c017d9b8.js";import"./index-6dd67f75.js";export{m as default};
---
+++ docs/vuetify-studio/assets/Convert-c95953e6.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./Convert.vue_vue_type_script_setup_true_lang-1cb77427.js";import"./index-2f6690f9.js";export{m as default};
