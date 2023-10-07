e07d8e4f6aa5b37a0d73957d4c74adae896df194
kieuminhcanh <minhcanhdn0704@gmail.com>
2023-10-07 11:11:01 -0700

    Update example

--- vuetify-studio/assets/Convert-9a76ac7f.js
+++
@@ -1,1 +0,0 @@
-import{_ as m}from"./Convert.vue_vue_type_script_setup_true_lang-f484ecee.js";import"./index-7d899cba.js";export{m as default};
--- vuetify-studio/assets/Empty-a9254dc2.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./_plugin-vue_export-helper-c27b6911.js";import{r as o,o as r,c as _,w as n,a as c,V as s}from"./index-7d899cba.js";const a={};function p(l,m){const e=o("router-view");return r(),_(s,null,{default:n(()=>[c(e)]),_:1})}const i=t(a,[["render",p]]);export{i as default};
